5eea9ec44809f7b9a7ed7db050731840f4def1d7
dqc <doquyetchiennd@gmail.com>
2025-07-24 10:27:31 +0700

    add image page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,16 @@
+import { AppImage } from "@/components/common";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <AppImage
+        src={`${process.env.NEXT_PUBLIC_DOMAIN}/uploads_v2/system/nam-moi-2025.jpg`}
+        width={500}
+        height={500}
+      />
+    </div>
+  );
 };
 
 export default page;
